042d7e7c38e5c7ea68056dc5a1a54ce3e13185d8
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-06-21 16:49:51 -0500

    feat: created a reject/approval card for my comments

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
-            <CommentDetail />
+            <ApprovalCard>
+                <CommentDetail
+                    author="Josh"
+                    timeAgo="Today 4:45PM"
+                    comment="I love this post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Ted"
+                    timeAgo="Today at 2:00AM"
+                    comment='the single best post!'
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jimmie"
+                    timeAgo="Yesterday at 9:23PM"
+                    comment="great post man!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
