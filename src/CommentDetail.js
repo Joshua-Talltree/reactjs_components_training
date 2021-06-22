ebc7bddc4453065c6b94d55e43054559663f7abf
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-06-21 23:29:44 -0500

    feat: added children to the prop file to make better component.

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 const CommentDetail = props => {
     return (
-        <div className="comment">
-            <a href="/">
-                <img alt="avatar" src={props.avatar} />
-            </a>
-            <div className="content">
-                <a href="/" className="author">
-                    {props.author}
+        <div className="ui container comment">
+            <div className="comment">
+                <a href="/">
+                    <img alt="avatar" src={props.avatar}/>
                 </a>
-                <div className="metadata">
-                    <span className="date">{props.timeAgo}</span>
+                <div className="content">
+                    <a href="/" className="author">
+                        {props.author}
+                    </a>
+                    <div className="metadata">
+                        <span className="date">{props.timeAgo}</span>
+                    </div>
+                    <div className="text">{props.comment}</div>
                 </div>
-                <div className="text">{props.comment}</div>
             </div>
         </div>
     );
